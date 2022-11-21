2959c36cacf927cdd0e6c3d8fb6b8f135c4e8358
sopanhahem <sopanhahem@gmail.com>
2022-11-21 22:08:49 +1100

    third commit

--- venv/src/videos/models.py
+++ venv/src/videos/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
-    title = models.CharField()
-    description = models.TextField()
+    title = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True) #'this-is-my-video'
-    video_id = models.CharField()
+    video_id = models.CharField(max_length=220)
     #timestamp
     #updated
     #state
     #publish_timestamp
+
+class VideoProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
+        
--- venv/src/videos/tests.py
+++ venv/src/videos/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from .models import Video
+
+class VideoModelTestCase(TestCase):
+    def setUp(self):
+        Video.objects.create(title='This is my title')
+
+    def test_valid_title(self):
+        title='This is my title'
+        qs = Video.objects.filter(title=title)
+        self.assertEqual(qs.count(), 1)
+
+
 # Create your tests here.
